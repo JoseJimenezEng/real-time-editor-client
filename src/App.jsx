720b4faf60ef389e217596771245d43f62e414fd
JoseJimenezEng <jjmarts@gmail.com>
2024-08-18 23:40:09 -0500

    Test env var

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 const socket = io(import.meta.env.REACT_APP_SERVER_URL);
+console.log(import.meta.env.REACT_APP_SERVER_URL)
 
 function App() {
   const [text, setText] = useState('');
